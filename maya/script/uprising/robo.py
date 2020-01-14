21916ce76da4cc286e904362a4dab4741a9c34ac
Julian Mann <julian.mann@gmail.com>
2020-01-14 10:41:43 -0500

    wip new singleton

--- maya/script/uprising/robo.py
+++ maya/script/uprising/robo.py
@@ -1,116 +1,110 @@
 import os
-
+import time
 from robolink import Robolink, ITEM_TYPE_ROBOT, ITEM_TYPE_TARGET
 
+
 CLEAN_FILE = os.path.join(os.environ["UPRISING_PROJECT_PATH"], "robodk", "clean.rdk")
+ROBODK_PATH = "/Applications/RoboDK/RoboDK.app/Contents/MacOS/RoboDK"
 
+class Singleton:
+    """
+    A non-thread-safe helper class to ease implementing singletons.
+    This should be used as a decorator -- not a metaclass -- to the
+    class that should be a singleton.
 
-def create_link(**kw):
-    startup_args = []
-    if kw.get("mode", "show") == "hidden":
-        startup_args = ["-HIDDEN", "-NOSPLASH", "-NOSHOW"]
-    if kw.get("debug"):
-        startup_args.append("-DEBUG")
+    The decorated class can define one `__init__` function that
+    takes only the `self` argument. Also, the decorated class cannot be
+    inherited from. Other than that, there are no restrictions that apply
+    to the decorated class.
 
-    print "startup_args", startup_args
-    path = "/Applications/RoboDK/RoboDK.app/Contents/MacOS/RoboDK"
+    To get the singleton instance, use the `instance` method. Trying
+    to use `__call__` will result in a `TypeError` being raised.
 
-    result = Robolink(args=startup_args, robodk_path=path)
+    """
 
-    print "Created Robo link"
-    print result
-    return result
+    def __init__(self, decorated):
+        self._decorated = decorated
 
-
-class Robo:
-    """Singleton to keep some common data accessible."""
-    instance = None
-
-    @classmethod
-    def clear(cls):
-        cls.instance = None
-
-# INIT 
-# ROBO IS NONE
-# instance WAS NOT NONE - DOINT NOTHING
-# instance is now <uprising.robo.__Robo instance at 0x15ac33ab8>
-# LINK AND ROBOT:
-# <robolink.Robolink instance at 0x15ac33b00>
-# RoboDK item (4562391056) of type 2
-# <robolink.Robolink instance at 0x15ac33b00>
-
-
-    class __Robo:
-        def __init__(self, **kw):
-            # This is called once
-            # self.mode =   kw.get("mode", "hidden") == "api"
-
-            self._link = create_link(**kw)
-            self._link.AddFile(CLEAN_FILE)
-            self._robot = self._link.Item("", ITEM_TYPE_ROBOT)
-            self._robot.setParam("PostProcessor", "KUKA KRC4_RN")
-            print "Got link and robot"
-            print "Loaded Clean scene and set PostProcessor to KUKA KRC4_RN"
-    
-
-    def __init__(self, **kw):
-        """Create a new Robo the first time only.
+    def instance(self):
         """
-        if kw.get("force"):
-            self.close()
-        # print "INIT "
-        if not Robo.instance:
-            # print "instance IS NONE - INSTANTIATING"
-            Robo.instance = Robo.__Robo(**kw)
-        # else:
-        #     print "instance WAS NOT NONE - DOINT NOTHING"
-        # print "instance is now", Robo.instance
-        # print "LINK AND ROBOT:"
-        # print Robo.instance._link
-        # print Robo.instance._robot
-        
-            
+        Returns the singleton instance. Upon its first call, it creates a
+        new instance of the decorated class and calls its `__init__` method.
+        On all subsequent calls, the already created instance is returned.
 
-    @property
-    def link(self):
-        return Robo.instance and Robo.instance._link
+        """
+        try:
+            return self._instance
+        except AttributeError:
+            self._instance = self._decorated()
+            return self._instance
 
-    @property
-    def robot(self):
-        if Robo.instance  and Robo.instance._link:
-            return Robo.instance._robot
+    def __call__(self):
+        raise TypeError('Singletons must be accessed through `instance()`.')
+
+    def __instancecheck__(self, inst):
+        return isinstance(inst, self._decorated)
+
+
+# @Singleton
+class Robo:
+    def __init__(self, **kw):
+        print 'Robo created'
+        self.link = None
+        self.robot = None
+
+        self.fresh(**kw)
+
+    def fresh(self, **kw):
+
+        self.close()
+        # time.sleep(1)
+
+        startup_args = []
+        if kw.get("mode", "show") == "hidden":
+            startup_args = ["-HIDDEN", "-NOSPLASH", "-NOSHOW"]
+        if kw.get("debug"):
+            startup_args.append("-DEBUG")
+
+        print "startup_args", startup_args
+        self.link = Robolink(args=startup_args, robodk_path=ROBODK_PATH)
+        self.link.AddFile(CLEAN_FILE)
+        self.robot = self.link.Item("", ITEM_TYPE_ROBOT)
+        self.robot.setParam("PostProcessor", "KUKA KRC4_RN")
+
+    def empty(self):
+        try:
+            for station in self.link.getOpenStations():
+                station.Delete()
+        except:
+            print "No stations to empty"
 
 
     def close(self):
-        if Robo.instance and Robo.instance._link:
-            try:
-                for station in Robo.instance._link.getOpenStations():
-                    station.Delete()
-                Robo.instance._link.CloseRoboDK()
-            except:
-                print "RoboDK not open"
-        Robo.clear()
+        self.empty()
+        try:
+            self.link.Disconnect()
+            self.link.CloseRoboDK()
+        except BaseException:
+            print "Already closed"
+        self.link = None
+        self.robot = None
 
     def show(self):
-        if Robo.instance and Robo.instance._link:
-             Robo.instance._link.ShowRoboDK()
+        self.link.ShowRoboDK()
 
     def hide(self):
-        if Robo.instance and Robo.instance._link:
-            Robo.instance._link.HideRoboDK()
-    
+        self.link.HideRoboDK()
+
     def clean(self):
-        if Robo.instance and Robo.instance._link:
-            for station in Robo.instance._link.getOpenStations():
-                print "Deleting existing station"
-                station.Delete()
-        Robo.instance._link.AddFile(CLEAN_FILE)
+        self.empty()
+        self.link.AddFile(CLEAN_FILE)
         print "Added clean file: {}".format(CLEAN_FILE)
 
-    def __str__(self):
-        return  repr(Robo.instance)
+
 
 
 # rodk = Robo()
 # robot = rodk.robot
 # rlink = rodk.link
+
+
