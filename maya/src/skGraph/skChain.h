e5b8ad41bf5c55bf1c7920a284ed392e5da80988
Julian Mann <julian.mann@gmail.com>
2020-01-22 02:52:38 -0500

    tidy

--- maya/src/skGraph/skChain.h
+++ maya/src/skGraph/skChain.h
@@ -12,7 +12,6 @@
 class skChain
 {
 public:
-
 	skChain();
 
 	~skChain();
@@ -25,37 +24,24 @@ public:
 
 	void appendPointsTo(MVectorArray &positions) const;
 
-	// int getContacts( MDoubleArray &contacts, double maxWidth) const;
+	const skPoint &operator[](const int index) const;
 
+	void appendRadiiTo(MFloatArray &radii) const;
+	void appendRadiiTo(MDoubleArray &radii) const;
 
-	// int getContacts(
-	//   MDoubleArray &contacts,
-	//   const std::vector< std::pair<int, float> > &widths) const;
-
-
-	const skPoint &operator[] (const int index) const ;
-
-
-
-
-	void appendRadiiTo( MFloatArray &radii) const;
-	void appendRadiiTo( MDoubleArray &radii) const;
-
-	void appendParamsTo( MDoubleArray &params) const;
+	void appendParamsTo(MDoubleArray &params) const;
 
 	void interpolate(int step, skChain &result) const;
-
 
 	const float &maxRadius() const;
 	const float &minRadius() const;
 
-	const std::vector< skPoint > &points() const;
+	const std::vector<skPoint> &points() const;
 
 private:
-	std::vector< skPoint > m_points;
+	std::vector<skPoint> m_points;
 
 	float m_maxRadius;
 	float m_minRadius;
-
 };
 #endif
