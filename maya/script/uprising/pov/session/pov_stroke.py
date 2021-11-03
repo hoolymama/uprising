3828cc2a82a4baf0f78a2f830c4c907afa9336e5
Julian Mann <julian.mann@gmail.com>
2021-11-03 09:46:04 -0500

    add a first linear stroke

--- maya/script/uprising/pov/session/pov_stroke.py
+++ maya/script/uprising/pov/session/pov_stroke.py
@@ -3,7 +3,8 @@ from robolink import (
     INSTRUCTION_SHOW_MESSAGE,
     INSTRUCTION_INSERT_CODE
 )
-
+from uprising import robo
+from uprising.utils import ClusterError
 from uprising.pov.session.pov_target import PovTarget
 import pymel.core as pm
 
@@ -21,12 +22,15 @@ class PovStroke(Stroke):
         positions = self.query_positions()
         rotations = self.query_rotations()
         colors = self.query_colors()
+
+
         self.targets = self._build_target_array(
             positions, rotations, colors, self.brush, self.id)
 
     @classmethod
     def _build_target_array(cls, positions, rotations, colors, brush, stroke_id):
         result = []
+        
         num_targets = len(positions)
         if not (num_targets == len(rotations) and num_targets == len(colors)):
             raise utils.StrokeError(
@@ -34,6 +38,9 @@ class PovStroke(Stroke):
 
         for i, (p, r, c) in enumerate(zip(positions, rotations, colors)):
             try:
+                if i == 0:
+                    tg = PovTarget(i, (p * 10), r, pm.dt.Color(0.0,0.0,0.0,0.0), brush)
+                    result.append(tg)
                 tg = PovTarget(i, (p * 10), r, c, brush)
             except utils.StrokeError:
                 print "Target Position:", p
@@ -55,11 +62,19 @@ class PovStroke(Stroke):
 
         for target in self.targets:
             target.configure(config)
-        self.targets[0].linear = False
+        self.targets[0].linear = False  
 
     def send(self, prefix, program, frame, motion):
+        link = robo.link()
+        tool = link.Item(self.brush.name)
+        if not tool.Valid():
+            raise ClusterError(
+                "SERIOUS RISK OF DAMAGE! Can't find valid tool!")
+
         stroke_name = self.name(prefix)
         program.RunInstruction("Stroke {}".format(stroke_name), INSTRUCTION_COMMENT)
+        program.setPoseTool(tool)
+        
         last_color = None
         for i, t in enumerate(self.targets):
             t.send(stroke_name, program, frame, last_color)
@@ -140,3 +155,9 @@ class PovStroke(Stroke):
                 strokeColors=True
             )
         )
+ 
+        # Now we want to make thge first color black, and all the others a blend.
+        # result = [pm.dt.Color(0.0,0.0,0.0,0.0)]
+        # for i in range(0, len(colors)-1):
+        #     result.append(colors[i])
+        # return result
