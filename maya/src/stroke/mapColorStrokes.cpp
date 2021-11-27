a8fa6ca6d4dde04f88aa8ec0b35d708875a68c4d
Julian Mann <julian.mann@gmail.com>
2021-11-27 12:53:04 -0500

    small bug in mapColorStrokes - rgb->white

--- maya/src/stroke/mapColorStrokes.cpp
+++ maya/src/stroke/mapColorStrokes.cpp
@@ -201,7 +201,7 @@ void mapColorStrokes::getColors(
 
     st = TexUtils::sampleSolidTexture(
         thisObj,
-        mapColorStrokes::aRGB,
+        mapColorStrokes::aWhite,
         1.0,
         points,
         whites);
