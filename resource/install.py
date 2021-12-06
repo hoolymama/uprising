f303d0c8a8fa3f2391d0a828ade2c2a52dc19be0
Julian Mann <julian.mann@gmail.com>
2021-12-05 20:30:57 -0500

    fix buggy install script

--- resource/install.py
+++ resource/install.py
@@ -10,24 +10,25 @@ from shutil import copy2
 
 # /something/uprising/5.1.0
 PKG_DIR =  os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MODULE_NAME = os.path.basename(os.path.dirname(PKG_DIR))
 MODULE_DIR = os.path.join(PKG_DIR, "maya")
 VERSION = os.path.basename(PKG_DIR)  # 5.1.0
-MODULE_NAME = os.path.dirname(PKG_DIR)
 SUPPORTED_MAYA_VERSIONS = [2019]
+MAYA_MODULES_DIR = os.path.expanduser("~/Library/Preferences/Autodesk/maya/modules")
 
 def main():
-    module_dir = os.path.expanduser("~/Library/Preferences/Autodesk/maya/modules")
+    ensure_directory(MAYA_MODULES_DIR)
+
     robodk_python_dir = os.path.expanduser("~/RoboDK/RoboDK.app/Contents/Python")
 
 
-    ensure_directory(module_dir)
-    fn = os.path.join(module_dir, "{}.mod".format(MODULE_NAME))
-    with open(fn, "w") as f:
+    fname = os.path.join(MAYA_MODULES_DIR, "{}.mod".format(MODULE_NAME))
+    with open(fname, "w") as f:
         for maya_version in SUPPORTED_MAYA_VERSIONS:
             f.write("+ MAYAVERSION:{} {} {} {}\n".format(maya_version, MODULE_NAME, VERSION, MODULE_DIR))
             f.write("PYTHONPATH+={}\n\n".format(robodk_python_dir))
 
-    sys.stdout.write("Completed Maya Module setup!\n")
+    sys.stdout.write("Completed Maya Module setup! '{}'\n".format(fname))
 
 
 def ensure_directory(directory):
