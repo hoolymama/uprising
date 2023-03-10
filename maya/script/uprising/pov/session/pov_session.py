85ce6e591f1794ce0987c3e141fbfa2731e1903a
Julian Mann <julian.mann@gmail.com>
2023-03-09 22:40:02 -0500

    directory is template

--- maya/script/uprising/pov/session/pov_session.py
+++ maya/script/uprising/pov/session/pov_session.py
@@ -5,6 +5,8 @@ import time
 import os
 import tarfile
 import pymel.core as pm
+import subprocess
+import shlex
 
 # from uprising.pov.session.pov_target import PovTarget
 
@@ -18,9 +20,9 @@ from uprising.pov.session.pov_target import PovTarget
 import maya.api.OpenMaya as om
 from uprising.brush import Brush
 
-RUNMODE_OFF=1
-RUNMODE_OFFLINE=2
-RUNMODE_ROBOT=3
+RUNMODE_OFF = 1
+RUNMODE_OFFLINE = 2
+RUNMODE_ROBOT = 3
 
 logger = logging.getLogger(__name__)
 
@@ -38,27 +40,23 @@ class PovSession(Session):
         start_frame,
         end_frame,
         pause,
+        directory,
+        post_cmd,
         program_prefix="pv",
     ):
-
         self.frame_range = list(range(start_frame, end_frame + 1))
         self.program_prefix = program_prefix
         self.stroke_chunk_size = stroke_chunk_size
         self.pause = pause
 
         self.run_mode = run_mode
-        self.save_rdk = save_rdk and run_mode==RUNMODE_OFFLINE
-        self.save_src = save_src and run_mode==RUNMODE_OFFLINE
+        self.save_rdk = save_rdk and run_mode != RUNMODE_OFF
+        self.save_src = save_src and run_mode == RUNMODE_OFFLINE
         self.save_maya_scene = save_maya_scene
         self.save_snapshots = save_snapshots
         self.notes = notes
-
-        self.directory = None
-        if self.save_rdk or self.save_src or self.save_maya_scene or self.save_snapshots:
-            self.directory = self.choose_session_dir()
-            if not self.directory:
-                print("No file chosen. Aborted")
-                raise ValueError("No valid directory chosen.")
+        self.directory = directory
+        self.post_cmd = post_cmd
 
         self.painting_node = pm.PyNode("lightPaintingShape")
 
@@ -67,51 +65,74 @@ class PovSession(Session):
     def run(self):
         timer_start = time.time()
         all_program_names = []
-        if self.save_maya_scene:
-            self.write_maya_scene(self.directory, "scene")
-        if self.notes:
-            self.write_text(self.directory, "notes", self.notes)
-            
+        if self.directory:
+            if self.save_maya_scene:
+                self.write_maya_scene(self.directory, "scene")
+            if self.notes:
+                self.write_text(self.directory, "notes", self.notes)
 
+        robo.new()
+        robo.hide()
         run_on_robot = self.run_mode == RUNMODE_ROBOT
         for frame in self.frame_range:
             pm.currentTime(frame)
             program_prefix = "{}_f{:03d}".format(self.program_prefix, frame)
             if self.save_snapshots:
-                kwargs={}
+                kwargs = {}
                 self.write_snapshot(self.directory, "snapshot", **kwargs)
 
-            
             if self.run_mode == RUNMODE_OFF:
                 continue
-            
-            robo.new()
-            robo.hide()
+            # robo.new()
+            robo.clean("kr8")
+            logger.debug("Clean scene")
+            # robo.hide()
+            # robo.clean("kr8")
 
+            #  Don't chunk if we are running on robot. We need all the instructions in one RDK file.
             stroke_count = pm.lightPaintingQuery(self.painting_node, sc=True)
-            if self.save_rdk or self.save_src:
+            if self.run_mode == RUNMODE_OFFLINE and (self.save_rdk or self.save_src):
                 stroke_chunk_size = self.stroke_chunk_size or stroke_count
             else:
                 stroke_chunk_size = stroke_count
 
-            robo.clean("kr8")
-            
+            logger.debug("Stroke count: {}".format(stroke_count))
+
             program = PovProgram(program_prefix, run_on_robot, self.pause)
             if not (program and program.painting and program.painting.strokes):
                 raise ValueError("Invalid pov_program. No painting")
 
             program.configure()
-
+            logger.debug("Program configured")
             # SEND
-            program_names = self.send_and_publish_pov_program(program, stroke_chunk_size)
+            program_names = self.send_and_publish_pov_program(
+                program, stroke_chunk_size
+            )
             all_program_names.extend(program_names)
+            logger.debug("End of frame {}".format(frame))
 
-        if len(all_program_names) > 1:
+        if len(all_program_names) > 1  and self.save_src:
             self.orchestrate(self.directory, all_program_names)
+
+        if self.post_cmd:
+            logger.info("Running post command: '{}'".format(self.post_cmd))
+            args = shlex.split(self.post_cmd)
+            completed = subprocess.run(args, check=True)
+            logger.info(
+                "Post command completed with return code {}".format(
+                    completed.returncode
+                )
+            )
+            if completed.stdout:
+                logger.info("Post command stdout:\n{}".format(completed.stdout))
+            if completed.stderr:
+                logger.warning("Post command Error:\n{}".format(completed.stderr))
 
         duration = int(time.time() - timer_start)
         progress.update(
-            major_line="Total time: {}".format(str(datetime.timedelta(seconds=duration))),
+            major_line="Total time: {}".format(
+                str(datetime.timedelta(seconds=duration))
+            ),
             minor_line="",
             major_progress=0,
             minor_progress=0,
