c1a4e5e085cf74125fbc767b03cde5a161b79b6d
Julian Mann <julian.mann@gmail.com>
2023-03-25 17:47:44 -0500

    basic quantize and dither

---
+++ maya/src/cimg/cImgQuantize.h
@@ -0,0 +1,50 @@
+
+#ifndef _cImgQuantize
+#define _cImgQuantize
+
+#include <maya/MPxNode.h>
+#include <maya/MTypeId.h>
+#include <maya/MTime.h>
+#include <maya/MColor.h>
+#include <math.h> 
+
+#include "cImgData.h"
+
+class cImgQuantize : public MPxNode
+{
+public:
+
+
+	cImgQuantize();
+
+	virtual ~cImgQuantize();
+
+	virtual MStatus		compute( const MPlug &plug, MDataBlock &data );
+
+	static  void		*creator();
+
+	static MStatus initialize();
+
+	static MObject aInput;
+	static MObject aOutput;
+	static MObject aLevels;
+	static MObject aGreyscale;
+
+	static MObject aDither;
+	
+	static MTypeId	id;
+
+    MStatus process(MDataBlock &data, const CImg<unsigned char> &image,
+	                        CImg<unsigned char> &result);
+
+
+private:
+
+
+	enum Dither {kNone, kFloydSteinberg};
+
+
+
+};
+
+#endif
